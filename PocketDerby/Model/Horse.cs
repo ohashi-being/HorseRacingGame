beaac95568857863c7d59a5c80cd900a96d5d539
ohashi <ohashi@beingcorp.co.jp>
2026-03-02 11:38:02 +0900

    #602505 Horseクラスの実装
    ・Horseクラスを実装しました
    ・馬番を表すプロパティを定義しました
    ・名前を表すプロパティを定義しました
    ・運の良さを表すプロパティを定義しました
    ・スピードを表すプロパティを定義しました
    ・オッズを表すプロパティを定義しました

---
+++ PocketDerby/Model/Horse.cs
@@ -0,0 +1,36 @@
+﻿namespace PocketDerby.Model {
+    /// <summary>
+    /// 馬のクラス
+    /// </summary>
+    internal class Horse {
+        /// <summary>
+        /// 馬番
+        /// </summary>
+        public int Number { get; }
+        /// <summary>
+        /// 馬名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 運の良さ
+        /// </summary>
+        public int Luck { get; set; }
+        /// <summary>
+        /// スピード
+        /// </summary>
+        public int Speed { get; set; }
+        /// <summary>
+        /// オッズ
+        /// </summary>
+        public double Odds { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="vNumber">馬番</param>
+        public Horse(int vNumber) {
+            this.Number = vNumber;
+        }
+
+    }
+}
