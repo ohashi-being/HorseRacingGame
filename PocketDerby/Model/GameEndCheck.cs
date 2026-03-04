5d2dc7cf266da2ada14a3ea282dda117f6c6243c
ohashi <ohashi@beingcorp.co.jp>
2026-03-04 13:30:48 +0900

    #602515 ゲーム終了判定機能の実装
    ・所持金が100円未満か判定する処理を実装しました
    ・所持金が50000円以上か判定する処理を実装しました

---
+++ PocketDerby/Model/GameEndCheck.cs
@@ -0,0 +1,34 @@
+﻿namespace PocketDerby.Model {
+    /// <summary>
+    /// ゲームの終了条件を判定するクラス
+    /// </summary>
+    internal static class GameEndCheck {
+
+        /// <summary>
+        /// クリアとなる所持金の条件
+        /// </summary>
+        private const int C_ClearMoney = 50000;
+        /// <summary>
+        /// 破産となる所持金の条件
+        /// </summary>
+        private const int C_LoseMoney = 100;
+
+        /// <summary>
+        /// クリア条件、破産条件を満たしているか判定する
+        /// </summary>
+        /// <returns>各条件に合ったメッセージ</returns>
+        public static GameStateEnum CheckGameState(int vPlayerMoney) {
+
+            if (vPlayerMoney >= C_ClearMoney) {
+                return GameStateEnum.GameClear;
+            }
+
+            if (vPlayerMoney < C_LoseMoney) {
+                return GameStateEnum.GameOver;
+            }
+
+            return GameStateEnum.Continue;
+        }
+    }
+}
+
