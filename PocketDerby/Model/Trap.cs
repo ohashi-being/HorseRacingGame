2c20b0f627bda50740f5fe0ed05451024a499878
ohashi <ohashi@beingcorp.co.jp>
2026-03-06 13:40:24 +0900

    #602517 トラップクラスの実装
    ・プロパティのアクセサを修正しました
    ・引数名を修正しました
    ・ほかのクラスでインスタンスが作成できないようにコンストラクタのアクセス修飾子を修正しました
    ・トラップ発生時、回避時のメッセージを取得するメソッドを作成しました
    ・TrapEnumクラスを削除しました

--- PocketDerby/Model/Trap.cs
+++ PocketDerby/Model/Trap.cs
@@ -1,47 +1,57 @@
-﻿namespace PocketDerby.Model {
+﻿using System.Collections.Generic;
+
+namespace PocketDerby.Model {
     /// <summary>
     /// トラップクラス
     /// </summary>
     internal class Trap {
 
         /// <summary>
-        /// トラップの種類
-        /// </summary>
-        public TrapEnum Type { get; private set; }
-        /// <summary>
         /// スピード補正値
         /// </summary>
-        public double SpeedCorrection { get; private set; }
+        public double SpeedCorrection { get; }
         /// <summary>
         /// トラップ発生時のメッセージ
         /// </summary>
-        public string TriggerMessage { get; private set; }
+        private readonly string FTriggerMessage;
         /// <summary>
         /// トラップ回避時のメッセージ
         /// </summary>
-        public string AvoidMessage { get; private set; }
+        private readonly string FAvoidMessage;
 
         /// <summary>
         /// コンストラクタ
         /// </summary>
-        /// <param name="vType">トラップの種類</param>
-        /// <param name="vSpeedReduction">スピード補正値</param>
+        /// <param name="vSpeedCorrection">スピード補正値</param>
         /// <param name="vTriggerMessage">トラップ発生時のメッセージ</param>
         /// <param name="vAvoidMessage">トラップ回避時のメッセージ</param>
-        public Trap(TrapEnum vType, double vSpeedReduction, string vTriggerMessage, string vAvoidMessage) {
-            this.Type = vType;
-            this.SpeedCorrection = vSpeedReduction;
-            this.TriggerMessage = vTriggerMessage;
-            this.AvoidMessage = vAvoidMessage;
+        private Trap(double vSpeedCorrection, string vTriggerMessage, string vAvoidMessage) {
+            this.SpeedCorrection = vSpeedCorrection;
+            this.FTriggerMessage = vTriggerMessage;
+            this.FAvoidMessage = vAvoidMessage;
         }
 
-        public static readonly Trap HeadWind =
-            new Trap(TrapEnum.HeadWind, 0.9, "おっと！{0}が逆風にあおられた！", "{0}は逆風をものともしない！");
-        public static readonly Trap BadTrack =
-            new Trap(TrapEnum.BadTrack, 0.8, "おっと！{0}の足元の馬場が悪い！", "{0}は悪い馬場を巧みにかわした！");
-        public static readonly Trap Temper =
-            new Trap(TrapEnum.Temper, 0.7, "おっと！{0}の気性が荒くなった！", "{0}は冷静さを保っている！");
+        /// <summary>
+        /// トラップ発生時のメッセージを取得する
+        /// </summary>
+        /// <param name="vHorseName">馬の名前</param>
+        /// <returns>トラップ発生時のメッセージ</returns>
+        public string GetTriggerMessage(string vHorseName) => string.Format(FTriggerMessage, vHorseName);
 
-        public static readonly Trap[] AllTraps = new Trap[] { HeadWind, BadTrack, Temper };
+        /// <summary>
+        /// トラップ回避時のメッセージを取得する
+        /// </summary>
+        /// <param name="vHorseName">馬の名前</param>
+        /// <returns>トラップ回避時のメッセージ</returns>
+        public string GetAvoidMessage(string vHorseName) => string.Format(FAvoidMessage, vHorseName);
+
+        public static readonly Trap HeadWind =
+            new Trap(0.9, "おっと！{0}が逆風にあおられた！", "{0}は逆風をものともしない！");
+        public static readonly Trap BadTrack =
+            new Trap(0.8, "おっと！{0}の足元の馬場が悪い！", "{0}は悪い馬場を巧みにかわした！");
+        public static readonly Trap Temper =
+            new Trap(0.7, "おっと！{0}の気性が荒くなった！", "{0}は冷静さを保っている！");
+
+        public static readonly IReadOnlyList<Trap> AllTraps = new Trap[] { HeadWind, BadTrack, Temper };
     }
 }
