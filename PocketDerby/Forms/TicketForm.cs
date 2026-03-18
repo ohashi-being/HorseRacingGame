7e8140fdfb67809f4a265ef24e40b69ea0465564
ohashi <ohashi@beingcorp.co.jp>
2026-03-18 16:39:30 +0900

    #602522-remake 掛け金入力画面の実装
    ・選択した馬の情報を表示しました
    ・所持金を表示しました
    ・掛け金を入力できるようにしました
    ・掛け金に応じた払戻金を表示しました

--- PocketDerby/Forms/TicketForm.cs
+++ PocketDerby/Forms/TicketForm.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using PocketDerby.Model;
 
 namespace PocketDerby {
-    public partial class TicketForm : Form {
-        public TicketForm() {
+    internal partial class TicketForm : Form {
+
+        /// <summary>
+        /// ゲーム全体を管理するGameManagerインスタンス
+        /// </summary>
+        private GameManager FGameManager;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="vGameManeger">ゲーム全体を管理するGameManagerインスタンス</param>
+        public TicketForm(GameManager vGameManeger) {
             InitializeComponent();
+
+            this.FGameManager = vGameManeger;
         }
 
         /// <summary>
